0cddc77d00747c60f7aa9d83714320da9d0afcfb
Brian Anthony Jacobe <brian.a.jacobe@gmail.com>
2024-01-10 22:55:21 -0800

    Implemented high score logic

--- GuessNumber/script.js
+++ GuessNumber/script.js
@@ -1,9 +1,9 @@
 "use strict";
 
 console.log(document.querySelector(".score").textContent);
-
+let highScore = 0;
 let playerScore = 20;
-const gameNumber = Math.trunc(Math.random() * 20 + 1);
+let gameNumber = Math.trunc(Math.random() * 20 + 1);
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -15,7 +15,10 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").textContent = gameNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
-
+    if (playerScore > highScore) {
+      highScore = playerScore;
+      document.querySelector(".highscore").textContent = highScore;
+    }
     // GUESS TOO HIGH
   } else if (guess > gameNumber) {
     if (playerScore > 1) {
@@ -41,8 +44,9 @@ document.querySelector(".check").addEventListener("click", function () {
 
 document.querySelector(".again").addEventListener("click", function () {
   playerScore = 20;
-  playerScore = Math.trunc(Math.random() * 20 + 1);
+  gameNumber = Math.trunc(Math.random() * 20 + 1);
   document.querySelector(".score").textContent = playerScore;
+  document.querySelector(".highscore").textContent = "0";
   document.querySelector(".guess").value = "";
   document.querySelector(".number").textContent = "?";
   document.querySelector(".message").textContent = "Start guessing...";
